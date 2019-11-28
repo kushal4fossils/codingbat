94fb948092e26df0303488e7d45750be840cfeed
kushal4fossils <kushal4fossils@gmail.com>
2019-11-28 17:54:38 +0530

    Update doubleChar.java

--- java/string-2/doubleChar.java
+++ java/string-2/doubleChar.java
@@ -2,15 +2,10 @@
  * there are two chars.
  */
 public String doubleChar(String str) {
-    char[] arr = new char[2 * str.length()];
-    int count = 0;
-      
-    for(int i = 0; i < str.length(); i++) {
-        arr[count] = str.charAt(i);
-        count++;
-        arr[count] = str.charAt(i);
-        count++;
-    }
-                            
-    return new String(arr);
+	String res = "";
+		for (int i = 0; i < str.length(); i++) {
+			char c = (char) (str.charAt(i));
+			res = res + c + c;
+		}
+		return res;
 }
